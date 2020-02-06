6d89536be3ebc210ffcc3f66b718153b21f641bb
Undoified <25871683+undoified@users.noreply.github.com>
2020-02-05 20:19:06 -0600

    Revert "Basic Drivetrain"

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -25,14 +25,13 @@ public class RobotMap {
   // public static int rangefinderModule = 1;
   
   //CAN
-  public static int rightFrontMotor = 1;
-  public static int rightBackMotor = 2;
-  public static int leftFrontMotor = 3;
-  public static int leftBackMotor = 4;
-  
+
   //DIO
   public static int[] encoderPorts = {0, 1};
   
-  //CAN
-
+  //PWM
+  public static int rightFrontMotor = 0;
+  public static int rightBackMotor = 1;
+  public static int leftFrontMotor = 2;
+  public static int leftBackMotor = 3;
 }
